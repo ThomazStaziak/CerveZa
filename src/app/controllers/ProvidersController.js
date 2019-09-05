7894c63acb5c57c619a7961090a4f3b8c10fbec9
ThomazStaziak <thostaziak@gmail.com>
2019-09-05 16:34:37 -0300

    fix: fixing the controller's name

--- src/app/controllers/ProvidersController.js
+++ src/app/controllers/ProvidersController.js
@@ -1,21 +1,24 @@
 module.exports = {
-	async index(req, res) {
-		res.send({ ok: true });
-	},
+  async index(req, res) {
+    res.render('providerForm.hbs', {
+      title: 'Cadastro de Fornecedor',
+      content: 'OI',
+    });
+  },
 
-	async show(req, res) {
-		res.send({ ok: true });
-	},
+  async show(req, res) {
+    res.send({ ok: true });
+  },
 
-	async store(req, res) {
-		res.send({ ok: true });
-	},
+  async store(req, res) {
+    res.send({ ok: true });
+  },
 
-	async update(req, res) {
-		res.send({ ok: true });
-	},
+  async update(req, res) {
+    res.send({ ok: true });
+  },
 
-	async remove(req, res) {
-		res.send({ ok: true });
-	},
+  async remove(req, res) {
+    res.send({ ok: true });
+  },
 };
