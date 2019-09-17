1def067863df84d7ffebc8c66abf27dd913152e1
NataliaLira <natalia.rlira@gmail.com>
2019-09-17 16:21:18 -0300

    chore: creating login and register views and routes

--- src/routes/staticPages.js
+++ src/routes/staticPages.js
@@ -5,11 +5,15 @@ const express = require('express');
 const routes = express.Router();
 
 // Declarando métodos dos controlles (desestruturado);
-const { home, about, contact } = require('../app/controllers/PagesController');
+const {
+  home, about, contact, login, register,
+} = require('../app/controllers/PagesController');
 
 // Criando rotas
 routes.get('/home', home);
 routes.get('/about', about);
 routes.get('/contact', contact);
+routes.get('/login', login);
+routes.get('/register', register);
 
 module.exports = routes;
