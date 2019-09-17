a05c2cbd3540e3c1ec81e05626d3ab8edb5aee6c
ThomazStaziak <thostaziak@gmail.com>
2019-09-17 17:01:30 -0300

    chore: adding the form-encoded-url and json parser

--- src/server.js
+++ src/server.js
@@ -19,20 +19,11 @@ const http = require('http');
 // Criando servidor
 const server = http.createServer(app);
 
-// Pondo servidor para ouvir na porta configurada no env
-server.listen(process.env.PORT);
-
 // Importando rotas
 const staticPages = require('./routes/staticPages');
 const customers = require('./routes/customers');
 const providers = require('./routes/providers');
 const products = require('./routes/products');
-
-// Criando as rotas da aplicação
-app.use('/pages', staticPages);
-app.use('/customers', customers);
-app.use('/providers', providers);
-app.use('/products', products);
 
 // Habilitando arquivos estáticos
 app.use('/public', express.static(path.join(__dirname, '/public')));
@@ -51,3 +42,16 @@ app.set('views', path.join(__dirname, './views'));
 
 // Especificando qual será a template engine
 app.set('view engine', 'hbs');
+
+// Configurando o servidor para receber informações do formulário
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Criando as rotas da aplicação
+app.use('/pages', staticPages);
+app.use('/customers', customers);
+app.use('/providers', providers);
+app.use('/products', products);
+
+// Pondo servidor para ouvir na porta configurada no env
+server.listen(process.env.PORT, console.log(`Listening on port ${process.env.PORT}`));
